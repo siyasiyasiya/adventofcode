644a7091ee4d04695c665c8a3b55263ccc1dc6da
Siya Jariwala <siyacj08@gmail.com>
2025-12-11 20:48:54 -0500

    solved day 4 part 1 & 2

--- 2025/day04/solution.py
+++ 2025/day04/solution.py
@@ -6,11 +6,11 @@ with open("input.txt", 'r') as f:
 rows = len(wall)
 cols = len(wall[0])
 
-print(rows)
-print(cols)
+remove = []
+
 def valid_remove(i, j):
     count = 0
-    if wall[i][j] == ".":
+    if wall[i][j] == "." or wall[i][j] == "x":
         return 0
     
     for dr in range(-1, 2):
@@ -19,12 +19,11 @@ def valid_remove(i, j):
                 continue
             
             nr, nc = i + dr, j + dc
-            if 0 <= nr < rows and 0 <= nc < cols:
-                if wall[nr][nc] == "@":
-                        count += 1
+            if 0 <= nr < rows and 0 <= nc < cols and wall[nr][nc] == "@":
+                    count += 1
 
     if count < 4:
-        print(i, j)
+        remove.append((i, j))
         return 1
 
     return 0
@@ -34,5 +33,21 @@ for i in range(0, rows):
     for j in range(0, cols):
         rolls += valid_remove(i, j)
 
+print(rolls)
+
+#part 2
+rolls = 0
+run = -1
+while not run == 0:
+    run = 0
+    remove = []
+    for i in range(0, rows):
+        for j in range(0, cols):
+            run += valid_remove(i, j)
+
+    for i, j in remove:
+        wall[i][j] = "x"
+
+    rolls += run
 
 print(rolls)
